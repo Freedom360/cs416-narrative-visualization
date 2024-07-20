70d954bf9c482bfb9e3c56549b2d5dea967833b8
Riya Shah <rshah257@illinois.edu>
2024-07-20 10:34:25 -0500

    testing scene 1

--- visualizations.js
+++ visualizations.js
@@ -1,7 +1,9 @@
-let currentScene = 1;
+var currentScene = 1;
+var unicornData = [];
 
 d3.csv("data/Unicorn_Clean.csv").then(data => {
     unicornData = data;
+    console.log('data loaded:', unicornData)
     updateScene();
 });
 
@@ -28,19 +30,22 @@ function updateScene() {
 // }
 
 function drawScene1() {
+    console.log('scene 1 start')
     d3.select("#visualization").html("");
 
     const svg = d3.select("#visualization").append("svg")
         .attr("width", 800)
         .attr("height", 600);
-
+    
+    console.log('data aggregated')
     // Aggregate data by country
     const countryData = d3.group(unicornData, d => d.country);
     const countries = Array.from(countryData, ([country, startups]) => ({ country, count: startups.length }));
 
     const x = d3.scaleBand().domain(countries.map(d => d.country)).range([0, 800]).padding(0.1);
     const y = d3.scaleLinear().domain([0, d3.max(countries, d => d.count)]).nice().range([600, 0]);
-
+    
+    console.log('making visual')
     svg.append("g")
         .selectAll("rect")
         .data(countries)
@@ -53,12 +58,14 @@ function drawScene1() {
 
     svg.append("g").call(d3.axisLeft(y));
     svg.append("g").attr("transform", "translate(0,600)").call(d3.axisBottom(x));
-
+    console.log('visual created')
     // Add annotations
     addAnnotations(svg, [
         { note: { label: "Highest count", title: "Country with most unicorns" }, x: x(countries[0].country), y: y(countries[0].count), dy: -30, dx: 30 }
     ]);
+    console.log('annotated')
 }
+console.log('viz 1 done')
 
 
 function drawScene2() {
