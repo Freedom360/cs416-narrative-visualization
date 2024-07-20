41b15e1485f9dd0a7d63f443ebe164a0d38275f9
Riya Shah <rshah257@illinois.edu>
2024-07-20 15:07:13 -0500

    testing map

--- visualizations.js
+++ visualizations.js
@@ -9,24 +9,66 @@ d3.csv("https://raw.githubusercontent.com/Freedom360/cs416-narrative-visualizati
   });
 
 
-// document.getElementById("prevButton").addEventListener("click", function() {
-//     if (currentScene > 1) currentScene--;
-//     updateScene();
-// });
+document.getElementById("prevButton").addEventListener("click", function() {
+    if (currentScene > 1) currentScene--;
+    updateScene();
+});
 
-// document.getElementById("nextButton").addEventListener("click", function() {
-//     if (currentScene < 4) currentScene++;
-//     updateScene();
-// });
+document.getElementById("nextButton").addEventListener("click", function() {
+    if (currentScene < 4) currentScene++;
+    updateScene();
+});
 
-// function updateScene() {
-//     console.log("Updating to scene:", currentScene);
-//     if (currentScene === 1) drawScene1();
-//     else if (currentScene === 2) drawScene2();
-//     else if (currentScene === 3) drawScene3();
-//     else if (currentScene === 4) drawScene4();
-// }
+function updateScene() {
+    console.log("Updating to scene:", currentScene);
+    if (currentScene === 1) drawScene1();
+    else if (currentScene === 2) drawScene2();
+    else if (currentScene === 3) drawScene3();
+    else if (currentScene === 4) drawScene4();
+}
 
-// function drawScene1() {
 
-// }
+
+function drawScene1() {
+    d3.select("#visualization").html("");
+
+    const width = 960;
+    const height = 600;
+
+    const svg = d3.select("#visualization").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    const projection = d3.geoMercator()
+        .scale(150)
+        .translate([width / 2, height / 1.5]);
+
+    const path = d3.geoPath().projection(projection);
+
+    const color = d3.scaleQuantize([0, d3.max(data, d => d.count)], d3.schemeBlues[9]);
+
+    const countryCounts = d3.rollup(data, v => v.length, d => d.country);
+
+    d3.json("data/world-110m.json").then(world => {
+        const countries = topojson.feature(world, world.objects.countries).features;
+
+        svg.append("g")
+            .selectAll("path")
+            .data(countries)
+            .enter().append("path")
+            .attr("fill", d => {
+                const count = countryCounts.get(d.properties.name) || 0;
+                return color(count);
+            })
+            .attr("d", path)
+            .append("title")
+            .text(d => `${d.properties.name}: ${countryCounts.get(d.properties.name) || 0} unicorns`);
+
+        svg.append("path")
+            .datum(topojson.mesh(world, world.objects.countries, (a, b) => a !== b))
+            .attr("class", "mesh")
+            .attr("d", path);
+    }).catch(error => {
+        console.error("Error loading the TopoJSON file:", error);
+    });
+}
